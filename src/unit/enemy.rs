cfe6f400b7d2152836a29bac3862df35d41de313
Smuli99 <hytonensamu99@gmail.com>
2026-02-18 14:26:36 +0200

    Added default struct for default to work

--- src/unit/enemy.rs
+++ src/unit/enemy.rs
@@ -1,1 +1,13 @@
-pub struct Enemy {}
+#[derive(Default)]
+#[allow(unused)]
+pub struct Enemy {
+    name: String,
+}
+
+impl Enemy {
+    pub fn new() -> Self {
+        Self {
+            name: "Thanos".to_string(),
+        }
+    }
+}
