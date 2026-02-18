22cb919e735c1f6d75ed9122994f24335a4a11b9
Smuli99 <hytonensamu99@gmail.com>
2026-02-18 15:58:25 +0200

    GameBuilder added and other methods

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use crate::unit::{Collectible, Enemy, Player, Wall};
+use crate::unit::{Collectible, Enemy, Player, PlayerBuilder, Wall};
 
 #[allow(unused)]
 pub struct Game {
@@ -10,11 +10,79 @@ pub struct Game {
 
 impl Game {
     pub fn new() -> Self {
+        Self::builder().build()
+    }
+
+    pub fn builder() -> GameBuilder {
+        GameBuilder::new()
+    }
+
+    fn init(&mut self) {
+        println!("Game initialized");
+        println!("Player speed: {}", self.player.speed());
+        println!("Player health: {}", self.player.health());
+        println!("Player is alive: {}", self.player.is_alive());
+        println!("Player takes {} damage...", self.player.health());
+        self.player.take_damage(self.player.health());
+        println!("Player is alive: {}", self.player.is_alive());
+    }
+
+    pub fn run(&mut self) {
+        self.init();
+    }
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+pub struct GameBuilder {
+    player: PlayerBuilder,
+    enemies: Vec<Enemy>,
+    walls: Vec<Wall>,
+}
+
+impl GameBuilder {
+    pub fn new() -> Self {
         Self {
-            enemies: vec![Enemy::default(), Enemy::default()],
-            walls: vec![Wall::default(), Wall::default(), Wall::default()],
+            player: PlayerBuilder::new(),
+            enemies: vec![
+                Enemy::with_speed(0.6),
+                Enemy::with_speed(0.5),
+                Enemy::with_speed(0.4),
+            ],
+            walls: vec![],
+        }
+    }
+
+    pub fn player_starting_health(mut self, health: u8) -> Self {
+        self.player = self.player.health(health);
+        self
+    }
+
+    pub fn player_starting_speed(mut self, speed: f64) -> Self {
+        self.player = self.player.speed(speed);
+        self
+    }
+
+    pub fn enemies(mut self, enemies: Vec<Enemy>) -> Self {
+        self.enemies = enemies;
+        self
+    }
+
+    pub fn walls(mut self, walls: Vec<Wall>) -> Self {
+        self.walls = walls;
+        self
+    }
+
+    pub fn build(self) -> Game {
+        Game {
+            enemies: self.enemies,
+            walls: self.walls,
             collectible: Collectible::default(),
-            player: Player::default(),
+            player: self.player.build(),
         }
     }
 }
