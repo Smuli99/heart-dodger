7a7575eaa6bbf66bfafa934d5baa951d18badcfd
Smuli99 <hytonensamu99@gmail.com>
2026-02-18 14:25:45 +0200

    Added filler struct for default to work

--- src/unit/collectible.rs
+++ src/unit/collectible.rs
@@ -1,1 +1,13 @@
-pub struct Collectible {}
+#[derive(Default)]
+#[allow(unused)]
+pub struct Collectible {
+    name: String,
+}
+
+impl Collectible {
+    pub fn new() -> Self {
+        Self {
+            name: "Collect me pleaseee...".to_string(),
+        }
+    }
+}
