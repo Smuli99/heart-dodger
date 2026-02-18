fd10ae593b2bedf8ccd8d39b118d95970bfa3b58
Smuli99 <hytonensamu99@gmail.com>
2026-02-18 14:27:07 +0200

    Added default struct for default to work

--- src/unit/wall.rs
+++ src/unit/wall.rs
@@ -1,1 +1,13 @@
-pub struct Wall {}
+#[derive(Debug, Default)]
+pub struct Wall {
+    pub name: String,
+}
+
+impl Wall {
+    // This will change
+    pub fn new() -> Self {
+        Self {
+            name: "I'm a wall!".to_string(),
+        }
+    }
+}
