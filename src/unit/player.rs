e84789f46da4010c075d005b27e2c86a5bdb2ef1
Smuli99 <hytonensamu99@gmail.com>
2026-02-18 14:26:13 +0200

    Added filler struct for default to work

--- src/unit/player.rs
+++ src/unit/player.rs
@@ -1,1 +1,13 @@
-pub struct Player {}
+#[derive(Debug, Default)]
+pub struct Player {
+    pub name: String,
+}
+
+impl Player {
+    // this will change
+    pub fn new() -> Self {
+        Self {
+            name: "Shlmatan".to_string(),
+        }
+    }
+}
