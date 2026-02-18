ff8267fa72652dd74f10bc838cbb16eee1d32fa9
Smuli99 <hytonensamu99@gmail.com>
2026-02-18 11:04:16 +0200

    Add unit module with Player, Enemy, Wall, Collectible structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod unit;
+
+#[allow(unused)]
+use crate::{ unit::Player, unit::Collectible, unit::Enemy, unit::Wall };
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/unit/mod.rs
@@ -0,0 +1,12 @@
+// Modules
+pub mod player;
+pub use player::Player;
+
+pub mod collectible;
+pub use collectible::Collectible;
+
+pub mod enemy;
+pub use enemy::Enemy;
+
+pub mod wall;
+pub use wall::Wall;
