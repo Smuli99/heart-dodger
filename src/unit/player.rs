dd873b5b123689d4ddadb1e691bbe3c9565cc400
Smuli99 <hytonensamu99@gmail.com>
2026-02-18 15:57:09 +0200

    PlayerBuilder added and other methods

--- src/unit/player.rs
+++ src/unit/player.rs
@@ -1,13 +1,69 @@
-#[derive(Debug, Default)]
+#[derive(Default)]
+#[allow(unused)]
 pub struct Player {
-    pub name: String,
+    speed: f64,
+    health: u8,
 }
 
 impl Player {
-    // this will change
+    pub fn new() -> Self {
+        Self::builder().build()
+    }
+
+    pub fn builder() -> PlayerBuilder {
+        PlayerBuilder::new()
+    }
+
+    pub fn is_alive(&self) -> bool {
+        self.health != 0
+    }
+
+    pub fn take_damage(&mut self, damage: u8) {
+        if damage >= self.health {
+            self.health = 0;
+        } else {
+            self.health -= damage;
+        }
+    }
+
+    pub fn health(&self) -> u8 {
+        self.health
+    }
+
+    pub fn speed(&self) -> f64 {
+        self.speed
+    }
+}
+
+
+// Player Builder
+pub struct PlayerBuilder {
+    speed: f64,
+    health: u8,
+}
+
+impl PlayerBuilder {
     pub fn new() -> Self {
         Self {
-            name: "Shlmatan".to_string(),
+            speed: 1.0,
+            health: 10,
+        }
+    }
+
+    pub fn speed(mut self, speed: f64) -> Self {
+        self.speed = speed;
+        self
+    }
+
+    pub fn health(mut self, health: u8) -> Self {
+        self.health = health;
+        self
+    }
+
+    pub fn build(self) -> Player {
+        Player {
+            speed: self.speed,
+            health: self.health,
         }
     }
 }
