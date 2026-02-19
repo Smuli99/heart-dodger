bdb3b262c76f868e54f114f1aaee28ef003f8b94
Smuli99 <hytonensamu99@gmail.com>
2026-02-19 23:05:06 +0200

    Added add, sub, addassign, mul and f64 impl

--- src/point.rs
+++ src/point.rs
@@ -1,6 +1,8 @@
 use num::traits::NumAssign;
 use crate::traits::Position;
 
+use std::ops::{Add, AddAssign, Sub, Mul};
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Point2d<T> {
     pub x: T,
@@ -22,6 +24,91 @@ impl<T: Default> Default for Point2d<T> {
     }
 }
 
+// for Point2d<f64>
+impl Point2d<f64> {
+    pub fn round(&self) -> Self {
+        Self {
+            x: self.x.round(),
+            y: self.y.round(),
+        }
+    }
+
+    pub fn to_u16(&self) -> Point2d<u16> {
+        Point2d {
+            x: self.x as u16,
+            y: self.y as u16,
+        }
+    }
+
+    pub fn normalize(&self) -> Self {
+        let magnitude = (self.x * self.x + self.y * self.y).sqrt();
+
+        // if dividing with zero
+        if magnitude == 0.0 {
+            return Self { x: 0.0, y: 0.0 };
+        }
+
+        Self {
+            x: self.x / magnitude,
+            y: self.y / magnitude,
+        }
+    }
+}
+
+// Add, AddAssign, Sub, Mul impl
+impl<T> Add for Point2d<T> 
+where
+    T: Add<Output = T> + Copy,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl<T> AddAssign for Point2d<T>
+where
+    T: AddAssign + Copy,
+{
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+impl<T> Sub for Point2d<T>
+where
+    T: Sub<Output = T> + Copy,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl<T> Mul<T> for Point2d<T>
+where
+    T: Mul<Output = T> + Copy,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+// Position impl
 impl<T> Position<T> for Point2d<T>
 where 
     T: NumAssign + Copy,
